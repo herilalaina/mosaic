9364bcaf52c5bf7d35ecff4b04403b9adf170c97
herilalaina <rkt.herilalaina@gmail.com>
2018-10-27 02:16:14 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+from package import Package
 
-setup(name='mosaic-config',
+setup(name='mosaic',
       version='0.1',
       description='General algorithm configurator',
       url='https://github.com/herilalaina/mosaic',
       author='Herilalaina Rakotoarison',
       author_email='heri@lri.fr',
       license='BSD-3',
-      packages=['mosaic'],
-      zip_safe=False)
+      packages=find_packages(),
+      include_package_data=True,
+      cmdclass={
+        "package": Package
+      }
+)
